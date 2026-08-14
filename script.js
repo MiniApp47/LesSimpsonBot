2f703cf3840148140cdf50df0e31d8c592cc48a4
PlugsBot <miniapp47@outlook.com>
2026-08-14 14:51:45 +0200

    new

--- script.js
+++ script.js
@@ -468,6 +468,23 @@ const appData = [
         quality: 'Coca ❄️',
         image: 'CategBl.png', 
         products: [
+            {
+                id: 'Diesel 2 ⛽',
+                flag: '🇨🇴',
+                name: 'Diesel 2 ⛽',
+                farm: 'No Farm',
+                promoEligible: true,
+                type: 'Hash',
+                image: 'ProductD2.jpg',
+                video: 'VideoD2.mp4',
+                description: '',
+                tarifs: [
+                    { weight: '1g', price: 80.00 },
+                    { weight: '2g', price: 160.00 },
+                    { weight: '5g', price: 350.00 },
+                    { weight: '10g', price: 500.00 }
+                ]
+            },
              {
                 id: 'Colombienne',
                 flag: '🇨🇴',
